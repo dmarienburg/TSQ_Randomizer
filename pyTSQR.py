dd89f6da20029b388528da69079829cfe891de85
David Katz-Wigmore <katzwigmore@gmail.com>
2020-10-31 15:14:49 -0700

    v.0.4b - lots of small fixes, basic functions work

--- pyTSQR.py
+++ pyTSQR.py
@@ -4,30 +4,46 @@ import random as rand
 from os import path
 
 class GameRandomizer:
-    def __init__(self, **kwargs):
-        self.owned_sets = {
-            "A Mirror in the Dark": True
-            "Total Ecliplse of the Sun": False,
-            "Risen from the Mire": True,
-            "At the Foundations of the World": False,
-            "Barricades": False,
-            "Ripples in Time": False,
-            "What Lies Beneath": False,
-            "Vengeful Sands": False,
-            "Clockwork Destiny": False
+    def __init__(self, *args):
+        self.args_lookup = {
+            "quest_1": "A Mirror in the Dark",
+            "quest_2": "Total Eclipse of the Sun",
+            "quest_3": "Risen from the Mire",
+            "quest_4": "At the Foundations of the World",
+            "quest_5": "Ripples in Time",
+            "quest_6": "What Lies Beneath",
+            "quest_7": "Frozen in Time",
+            "quest_8": "Vengeful Sands",
+            "quest_9": "Clockwork Destiny",
+            "barricades": "Barricades"
         }
+        self.owned_sets = {self.args_lookup[value]: True for value in args}
         self.cards = self.load_cards()
         self.deck_pool = {
             "Heros":{},
             "Weapons": {},
             "Spells": {},
             "Items": {},
-            "Treasures": {},
+            "Treasure": {},
             "Legendaries": {},
-            "Monsters": {},
-            "Dungeon Rooms": {}
+            "Monsters": {"1":{}, "2":{}, "3":{}},
+            "Dungeon Rooms": {"1":{}, "2":{}, "3":{}},
+            "Guardians": {}
         }
-        self.randomized_decks = {}
+        self.randomized_decks = {
+            "Heros":{},
+            "Weapons": {},
+            "Spells": {},
+            "Items": {},
+            "Treasure": {},
+            "Legendaries": {},
+            "Monsters": {"1":{}, "2":{}, "3":{}},
+            "Dungeon Rooms": {"1":{}, "2":{}, "3":{}},
+            "Guardians":{}
+        }
+
+        self.create_deck_pool()
+        self.select_from_pool()
     
     def create_deck_pool(self):
         """
@@ -45,6 +61,16 @@ class GameRandomizer:
             key for key in self.owned_sets.keys() if self.owned_sets[key] is True
         ]
 
+        # Iterate through the list of selected quests and fill the appropriate
+        # card categories in the self.deck_pool class variable
+        for quest in included_quests:
+            for key in self.deck_pool.keys():
+                if key in ["Monsters", "Dungeon Rooms"]:
+                    for level in ["1", "2", "3"]:
+                        self.deck_pool[key][level].update(self.cards["Quest"][quest][key][level])
+                else:
+                    self.deck_pool[key].update(self.cards["Quest"][quest][key])
+
     def load_cards(self):
         """
         Open the cards.json and load the results into the self.cards class
@@ -59,10 +85,10 @@ class GameRandomizer:
             "cards.json"
         )
         with open(data_path) as data_file:
-            data_dict = js.dump(data_file)
+            data_dict = js.load(data_file)
             data_file.close()
         
-        retrun data_dict
+        return data_dict
 
     def select_from_pool(self):
         """
@@ -71,5 +97,64 @@ class GameRandomizer:
 
         The results will be placed in the randomized_decks class var.
         """
-        pass
+        deck_counts = {
+            "Heros":{"need": 4, "current": 0},
+            "Market":{
+                "need": 8, 
+                "current": 0, 
+                "Items": 0, 
+                "Weapons": 0, 
+                "Spells": 0
+            },
+            "Legendaries": {"need": True, "current": 0},
+            "Monsters": {
+                "1":{"need": 1, "current": 0}, 
+                "2":{"need": 1, "current": 0}, 
+                "3":{"need": 1, "current": 0}
+            },
+            "Dungeon Rooms": {
+                "1":{"need": 2, "current": 0}, 
+                "2":{"need": 2, "current": 0}, 
+                "3":{"need": 2, "current": 0}
+            },
+            "Treasure":{},
+            "Guardians":{"need": 1, "current": 0}
+        }
+
+        for key in deck_counts.keys():
+            # Create Market decks list
+            if key == "Market":
+                while deck_counts[key]["current"] != deck_counts[key]["need"]:
+                    type = rand.choice(["Weapons", "Spells", "Items"])
+                    # make sure that no one market item type ever has more than
+                    # three decks.
+                    if deck_counts[key][type] == 3:
+                        pass
+                    else:
+                        choice = rand.choice(list(self.deck_pool[type].keys()))
+                        value = self.deck_pool[type].pop(choice)
+                        self.randomized_decks[type][choice] = value
+                        deck_counts[key]["current"] += 1
+                        deck_counts[key][type] += 1
+            # Create Dungeon Rooms and Monsteres lists
+            elif key in ["Monsters", "Dungeon Rooms"]:
+                for level in ["1", "2", "3"]:
+                    while deck_counts[key][level]["current"] != deck_counts[key][level]["need"]:
+                        choice = rand.choice(list(self.deck_pool[key][level].keys()))
+                        value = self.deck_pool[key][level].pop(choice)
+                        self.randomized_decks[key][level][choice] = value
+                        deck_counts[key][level]["current"] += 1
+            # Create Heros list
+            elif key == "Heros":
+                while deck_counts[key]["current"] != deck_counts[key]["need"]:
+                    choice = rand.choice(list(self.deck_pool[key].keys()))
+                    value = self.deck_pool[key].pop(choice)
+                    self.randomized_decks[key][choice] = value
+                    deck_counts[key]["current"] += 1
+            # Choose a random guardian
+            elif key == "Guardians": 
+                self.randomized_decks[key].update(self.deck_pool[key][rand.choice(list(self.deck_pool[key].keys()))])
+            # Add all Treasures and Legendaries cards
+            else:
+                self.randomized_decks[key].update(self.deck_pool[key])
     
